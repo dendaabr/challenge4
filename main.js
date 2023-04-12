f536a06eacde3432da584a4adaff7c76329c10bf
Denda Abdul Rohman <85669508+dendaabr@users.noreply.github.com>
2023-04-12 21:04:33 +0700

    fungsi get set dan pilihan player dan komputer

--- main.js
+++ main.js
@@ -6,8 +6,45 @@ class Start {
         this.comOption;
         this.wingame = ""
     }
+
+
+    get getYourOption() {
+        return this.yourOption
+    }
+    set setYourOption(option) {
+        this.yourOption = option;
+    }
+    get getcomOption() {
+        return this.comOption
+    }
+    set getcomOption(option) {
+        this.comOption = option;
+    }
+
+
+    result() {
+        if (this.yourOption == "✊" && this.comOption == "🖖") {
+            this.wingame = this.yourname
+        } else if (this.yourOption == "✊" && this.comOption == "🖐️") {
+            this.wingame = this.comname;
+        } else if (this.yourOption == "🖖" && this.comOption == "🖐️") {
+            this.wingame = this.yourname
+        } else if (this.yourOption == "🖖" && this.comOption == "✊") {
+            this.wingame = this.comname;
+        } else if (this.yourOption == "🖐️" && this.comOption == "✊") {
+            this.wingame = this.yourname
+        } else if (this.yourOption == "🖐️" && this.comOption == "🖖") {
+            this.wingame = this.comname;
+        } else {
+            return this.wingame = 'Draw'
+        }
+    }
 }
+
+
 
 function OptionPick(fingger) {
     const start = new Start();
+
+    console.log(start.yourOption);
 }
